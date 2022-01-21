f83440a455c8352be470eb26f758a2e16eb71db2
Aldis <aldisgailis1@gmail.com>
2022-01-21 06:12:09 -0800

    Update SearchShow.tsx

--- src/components/search/SearchShow.tsx
+++ src/components/search/SearchShow.tsx
@@ -1,44 +1,42 @@
-import { FormEvent, useState, useEffect, useRef } from "react"
-import { useAppDispatch } from "../../store/redux/hooks"
-import { searchInput } from "../../store/slices/searchShowSlice"
-import SearchIcon from "../icons/SearchIcon"
-import NavigateBtn from "../tools/buttons/NavigateBtn"
-import './searchShow.scss'
+import { FormEvent, useState, useEffect, useRef } from "react";
+import { useAppDispatch } from "../../store/redux/hooks";
+import { searchInput } from "../../store/slices/searchShowSlice";
+import SearchIcon from "../icons/SearchIcon";
+import NavigateBtn from "../tools/buttons/NavigateBtn";
+import "./searchShow.scss";
 
 const SearchShow = () => {
-  const dispatch = useAppDispatch()
-  const [userInput, setUserInput] = useState('')
+  const dispatch = useAppDispatch();
+  const [userInput, setUserInput] = useState("");
 
-  const focusOnInput = useRef<HTMLInputElement | null>(null)
+  const focusOnInput = useRef<HTMLInputElement | null>(null);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    dispatch(searchInput(userInput.trim()))
-  }
+    e.preventDefault();
+    dispatch(searchInput(userInput.trim()));
+  };
   useEffect(() => {
-    focusOnInput.current?.focus()
-  }, [])
+    focusOnInput.current?.focus();
+  }, []);
 
   return (
-    <form
-      className="search-show-container"
-      onSubmit={(e) => handleSubmit(e)}
-    >
+    <form className="search-show-container" onSubmit={(e) => handleSubmit(e)}>
+      <label className="search-show-label">
+        <input
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          ref={focusOnInput}
+          className="search-input"
+          type="text"
+          placeholder="Search for a show"
+        />
 
-      <input
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
-        ref={focusOnInput}
-        className="search-input"
-        type="text"
-        placeholder="Search for a show"
-      />
-
-      <NavigateBtn>
-        <SearchIcon />
-      </NavigateBtn>
+        <NavigateBtn>
+          <SearchIcon />
+        </NavigateBtn>
+      </label>
     </form>
-  )
-}
+  );
+};
 
-export default SearchShow
+export default SearchShow;
